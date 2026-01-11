f2c7ed5532ef056d2cbd5c16e65de86d37433907
Tareq Khalil <tareqkhalil1415@gmail.com>
2026-01-11 19:17:06 +0200

    Update Workers.jsx

--- src/Components/Workers.jsx
+++ src/Components/Workers.jsx
@@ -23,7 +23,7 @@ const roleMapping = {
   "yassin ahmed": "Co-Web Developer",
   "omar mohamed": "Vice-President",
   "omar sabry": "President",
-  "amro salah": "Sex Slave",
+  "amro salah": "Illustrator Mentor",
   "kozman ashraf": "Photoshop Mentor",
   "islam abdelsalam": "President",
   "shahd": "Mentor",
