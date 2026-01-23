b5fd44243c0b4a95496d725ce2b93074bda572dc
Tarek Khalil <tareqkhalil1415@gmail.com>
2026-01-23 19:40:26 +0200

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -131,7 +131,7 @@ function NavBar() {
               { to: "/", label: "Home", color: "#ec1a63", border: false },
               { to: "/about", label: "About us", color: "#fdef9d", border: false },
               { to: "/gallery", label: "Gallery", color: "#2caa7c", border: false },
-              { to: "/Bootcamp", label: "Bootcamp", color: "#FFFFFF", border: "#ec1a63" },
+              { to: "/Bootcamp", label: "Bootcamp", color: "#ec1a63", border: true },
             ].map((item) => (
               <Link
                 key={item.to}
